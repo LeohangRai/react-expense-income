71783e513b732b5c60667a7165de986acfd17501
Anup Rai <anup.rai@ekbana.info>
2023-10-03 21:31:15 +0545

    chore: code lint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:prettier/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -14,7 +15,8 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
-}
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  }
+};
