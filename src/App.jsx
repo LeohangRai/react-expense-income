a983303e3b1467e309effe59a52d7dcd6cc21c20
Anup Rai <anup.rai@ekbana.info>
2023-10-03 22:51:08 +0545

    feat: two-way binding for input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [type, setType] = useState('income');
+
   return (
     <main>
       <div>
         <h1 className="total-text">0</h1>
         <div className="input-container">
-          <input type="text" name="title" placeholder="Title" />
-          <input type="number" name="amount" placeholder="Amount" />
-          <select name="type">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            onClick={(e) => {
+              setAmount(e.target.value);
+            }}
+            value={amount}
+          />
+          <select
+            name="type"
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+            value={type}
+          >
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
